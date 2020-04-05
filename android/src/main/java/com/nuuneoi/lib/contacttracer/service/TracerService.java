d5e5c315ce5bf116bc7a52703c2e05ae66b1115e
Sittiphol Phanvilai <sittiphol@gmail.com>
2020-04-05 02:38:31 -0700

    Remove remote process from TracerService

--- android/src/main/java/com/nuuneoi/lib/contacttracer/service/TracerService.java
+++ android/src/main/java/com/nuuneoi/lib/contacttracer/service/TracerService.java
@@ -254,7 +254,6 @@ public class TracerService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        super.onStartCommand(intent, flags, startId);
         return START_STICKY;
     }
 
